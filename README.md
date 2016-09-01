0840e979a6d9eaacfcbb9afc18c8d66923acae11
JimTheMan <mrdotjim@gmail.com>
2016-08-31 22:51:59 -0400

    initial scaffold

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# My-Nightwatch-React-Movielist
-My own fork off of the original "movielist" project that uses Nightwatch in a React project.
+# MovieList
+
+###Getting Started###
+
+Checkout this repo, install dependencies, then start the gulp process with the following:
+
+```
+	> git clone git@github.com:jschuman/movie_list.git
+	> cd movie_list
+	> npm install
+	> npm start
+```
+
+To run e2e tests:
+
+```
+	> ./nightwatch
+```
---
+++ tests/movie_list.js
@@ -0,0 +1,101 @@
+describe('Movie Search Tests', () => {
+
+  describe('when conducting a search', () => {
+
+    beforeEach( (client, done) => {
+      client
+        .url('http://localhost:8080')
+        .waitForElementVisible('body', 1000)
+        .setValue('input.search-input', 'star wars')
+
+
+          // client.pause(12000);
+          client
+              .waitForElementVisible('button.search-btn', 1000)
+        .click('button.search-btn')
+
+
+      done();
+    });
+
+    xit ('shows results', (client) => {
+      client.expect.element('.table').to.contain.text('A New Hope');
+    });
+
+    it ('retains search string in search input field', (client) => {
+      client.expect.element('input.search-input').to.contain.value('star wars');
+    });
+
+    it ('shows results in ascending order by year by default', (client) => {
+      client.expect.element('table th.header-Year i.fa-sort-asc').to.be.present;
+      client.expect.element('table tr:first-child td.movie-Year').to.contain.text('1977');
+    });
+
+    xit ('shows results in descending order by year when year header first clicked', (client) => {
+      client.click('table th.header-Year');
+      client.expect.element('table th.header-Year i.fa-sort-desc').to.be.present;
+      client.expect.element('table tr:first-child td.movie-Year').to.contain.text('20');
+      client.getText("table tr:first-child td.movie-Year", function(result) {
+        this.assert.equal(result.value > 2010, true);
+      });
+    });
+
+    it ('shows results in ascending order by title when title he.waitForElementVisible(\'table tr:first-child td.movie-Year\', 500)ader first clicked', (client) => {
+      client.click('table th.header-Title');
+      client.expect.element('table th.header-Title i.fa-sort-asc').to.be.present;
+    });
+
+    it ('shows results in descending order by title when title header clicked twice', (client) => {
+      client.click('table th.header-Title');
+      client.click('table th.header-Title');
+      client.expect.element('table th.header-Title i.fa-sort-desc').to.be.present;
+    });
+
+    describe('and selecting a movie', () => {
+      beforeEach( (client, done) => {
+        client
+          .click('table tr:first-child td.movie-Title')
+          .waitForElementVisible('h3', 500)
+
+        done();
+      });
+
+      it ('has url with imdb id', (client) => {
+        //hardcoded with Star Wars id
+        client.assert.urlContains('/movies/tt0076759');
+      });
+
+      it ('shows the details of a movie', (client) => {
+        client.expect.element('h3 span').to.be.present;
+        client.expect.element('span#imdb_rating').to.contain.text('IMDB Rating');
+      });
+
+      it ('has a link back to search page', (client) => {
+        client.expect.element('a').to.have.attribute('href').equals('http://localhost:8080/');
+      });
+
+
+      //should only show \'Night of the Living Dead\' titled movies when I type that in
+     xit('gg', (client) => {
+
+        client
+            .setValue('input.search-input', 'Night of the Living Dead')
+            .waitForElementVisible('button.search-btn', 1000)
+            .click('button.search-btn')
+            .waitForElementVisible('table tr:first-child td.movie-Year', 500)
+            .expect.element('td#movie-Title').to.contain.text('Night of the Living Dead');
+
+      })
+
+
+
+    });
+
+  });
+
+  after( (client, done) => {
+    client.end();
+    done();
+  });
+
+});
